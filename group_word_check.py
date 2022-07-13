2f1a0368274cf9b440c3314bc7566fb9f97ecbf5
ohsuk kwon <percykwon@gmail.com>
2022-07-13 22:04:13 +0900

    second commit

--- group_word_check.py
+++ group_word_check.py
@@ -3,7 +3,7 @@ cnt = N
 
 for i in range(N):
     word = input()
-    for j in range(0, len(word)-1):
+    for j in range(len(word)-1):
         if word[j] == word[j+1]:
             pass
         elif word[j] in word[j+1:]:
