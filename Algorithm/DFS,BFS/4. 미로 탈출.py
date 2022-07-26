fabd3b721e804fc92d5e1f29f96a96dcb40e3072
ohsuk kwon <percykwon@gmail.com>
2022-07-26 17:09:07 +0900

    Docs : 4.미로 탈출 #2

--- Algorithm/DFS,BFS/4. 미로 탈출.py
+++ Algorithm/DFS,BFS/4. 미로 탈출.py
@@ -22,10 +22,10 @@ def bfs(x,y):
             nx = x + dx[i]
             ny = y + dy[i]
             # 맵을 벗어나거나 괴물이 있는 곳일 경우 무시
-            if x < 0 or y < 0 or x >= n or y >= m or graph[x][y] == 0:
+            if nx < 0 or ny < 0 or nx >= n or ny >= m or graph[nx][ny] == 0:
                 continue             
             # 해당 위치를 처음 방문하는 경우에만 거리 +1 기록
-            if graph[x][y] == 1:
+            if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
                 
