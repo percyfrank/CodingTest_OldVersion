84ba8b063ec3772ee0fbe3ebdd38f08c7fc9c0ba
ohsuk kwon <percykwon@gmail.com>
2022-07-26 18:41:25 +0900

    Docs : s3_바이러스 #2

--- BAEKJOON/그래프 탐색/s3_바이러스.py
+++ BAEKJOON/그래프 탐색/s3_바이러스.py
@@ -2,7 +2,10 @@
 n = int(input())
 m = int(input())
 
+# 0부터 시작하기 때문에 n+1
 graph = [[] for _ in range(n+1)]
+
+# 노드 연결 정보
 for _ in range(m):
     x,y = map(int,input().split())
     graph[x].append(y)
@@ -18,21 +21,8 @@ def dfs(graph,v,visited):
             
 dfs(graph,1,visited)
 
-count = -1
+count = -1 #자기 자신의 노드도 포함하기 때문에 0이 아닌 -1부터 시작
 for i in range(len(visited)):
     if visited[i] == True:
         count += 1
 print(count)
-        
-            
-            
-             
-    
-
-
-
-
-    
-    
-    
-    
