084ebc683432c4df8a36c5feb3926882950ec737
ohsuk kwon <percykwon@gmail.com>
2022-07-31 23:55:23 +0900

    s4_빙고 #1

---
+++ BAEKJOON/구현/s4_빙고.py
@@ -0,0 +1,21 @@
+
+check = [[0]*5 for _ in range(5)]
+
+array = []
+for i in range(5):
+    array.append(list(map(int,input().split())))
+
+bingo = []
+for i in range(5):
+    bingo.append(list(map(int,input().split())))
+
+    
+    
+print(array.index)    
+
+# for i in range(5):
+#     for j in range(5):
+#         m,n = array.index()
+#         check[m][n] = 1
+        
+            
