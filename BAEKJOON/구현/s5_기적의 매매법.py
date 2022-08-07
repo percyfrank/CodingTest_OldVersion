da1ee6c9f5a5cc6a90408e8cc11be374d3300484
ohsuk kwon <percykwon@gmail.com>
2022-08-07 22:50:52 +0900

    s5_기적의 매매법 #2

--- BAEKJOON/구현/s5_기적의 매매법.py
+++ BAEKJOON/구현/s5_기적의 매매법.py
@@ -1,13 +1,51 @@
 n = int(input())
+n1 = n
 
-a = []
-for i in range(14):
-    a.append(list(map(int,input().split())))
+a = list(map(int,input().split()))
     
-tmp = 0
-cnt = 0
+tmp1 = 0
 for i in range(len(a)):
-    tmp = n // a[i]
-    n -= tmp*a[i]
-    if n == 0:
+    if n1 == 0:
+        continue
+    tmp1 += n1 // a[i]
+    n1 -= tmp1 * a[i]
+    
+cnt1 = 0
+cnt2 = 0
+idx = a[0]
+tmp2 = 0
+for i in range(len(a)-1):
+    if a[i] < a[i+1]:
+        cnt2 = 0
+        idx = a[i+1]
+        cnt1 += 1
+    elif a[i] > a[i+1]:
+        cnt1 = 0
+        idx = a[i+1]
+        cnt2 += 1
+    else:
+        cnt1 = 0
+        cnt2 = 0
         
+    if cnt1 == 3:
+        n += idx * tmp2
+        cnt1 -= 1
+        tmp2 = 0
+    if cnt2 == 3:
+        if n <= 0:
+            continue
+        tmp2 += n // idx
+        cnt2 -= 1
+        if n // idx == 0:
+            continue
+        n -= (n // idx) * idx
+        
+bnp = tmp1 * a[13]+n1
+timing = tmp2 * a[13]+n
+
+if bnp > timing:
+    print("BNP")
+elif bnp < timing:
+    print("TIMING")
+else:
+    print("SAMESAME")
