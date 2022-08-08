88434aae60d0184d5db9a341e677ed4b3689d827
ohsuk kwon <percykwon@gmail.com>
2022-08-08 23:40:01 +0900

    g4_연구소 #1

---
+++ BAEKJOON/그래프 탐색/g4_연구소.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n,m = map(int,input().split())
+
+virus = [[0]*m for _ in range(n)]
+
+for i in range(n):
+    virus[i] = list(map(int,input().split()))
+    
+
+queue = deque()
+
+
+
+
+
+cnt = 0
+for i in range():
+    for j in range():
+        if virus[i][j] == 0:
+            cnt += 1
+
+print(cnt)
+    
