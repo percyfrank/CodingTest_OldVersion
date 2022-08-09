c82bd7ea539f5820af6a4f2f73905ac42e5af7e4
ohsuk kwon <percykwon@gmail.com>
2022-08-09 23:22:51 +0900

    g4_연구소 #2

--- BAEKJOON/그래프 탐색/g4_연구소.py
+++ BAEKJOON/그래프 탐색/g4_연구소.py
@@ -10,9 +10,25 @@ for i in range(n):
 
 queue = deque()
 
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
+def bfs():
+    for i in range(n):
+        for j in range(m):
+            if virus[i][j] == 2:
+                queue.append((i,j))
 
-
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if virus[nx][ny] == 0:
+                    virus[nx][ny] = 2
+                    queue.append((nx,ny))
+            
 
 cnt = 0
 for i in range():
