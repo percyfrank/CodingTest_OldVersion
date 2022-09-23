57b7d8e7eca36c63107f00a70284e4d895de2848
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-09-23 14:16:29 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 208 ms, Memory: 17608 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -6,22 +6,11 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int n = N;
-        int cnt = 0;
         int sum = 0;
-
-        // 입력된 N의 자릿수 구하기
-        while(true) {
-            n  = (int) n / 10;
-            cnt++;
-            if (n == 0) break;
+        
+        for (int i = 1; i <= N; i*=10) {
+            sum += (N-i)+1;
         }
-
-        for (int i = 1; i < cnt; i++) {
-            sum += (int) (i * (9 * Math.pow(10,i-1)));
-        }
-
-        sum += (int) (cnt * (N-Math.pow(10,cnt-1)+1));
 
         System.out.println(sum);
     }
