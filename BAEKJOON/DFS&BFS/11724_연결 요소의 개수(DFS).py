dd5b669d0b9ea7e51224b2ceb2fdd57a071669c8
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-17 10:34:55 +0900

    11724_연결 요소의 개수(DFS)

---
+++ BAEKJOON/DFS&BFS/11724_연결 요소의 개수(DFS).py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for _ in range(m):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(node):
+    visited[node] = True
+    for next in graph[node]:
+        if not visited[next]:
+            dfs(next)
+
+cnt = 0
+for i in range(1,n+1):
+    if not visited[i]:
+        dfs(i)
+        cnt += 1
+
+print(cnt)
+
+
+
+
+
