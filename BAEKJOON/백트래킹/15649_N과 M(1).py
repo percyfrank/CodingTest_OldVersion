396a6c18e731f5947447fc055bbefa1987f8c838
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-18 19:48:27 +0900

    15649_N과M(1) #2

--- BAEKJOON/백트래킹/15649_N과 M(1).py
+++ BAEKJOON/백트래킹/15649_N과 M(1).py
@@ -1,19 +1,17 @@
 
-
-def solve(depth,n,m):
-    if depth == m:
-        print(' '.join(map(str,answer)))
+def solve(start):
+    if len(answer) == m:
+        print(*answer)
         return
-    for i in range(1, n+1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        answer.append(i)
-        solve(depth+1,n,m)
-        visited[i] = False
-        answer.pop()
+    for num in range(1, n+1):
+        if not visited[num]:
+            visited[num] = True
+            answer.append(num)
+            solve(start)
+            visited[num] = False
+            answer.pop()
 
 n,m = map(int,input().split())
 visited = [False] * (n+1)
 answer = []
-solve(0,n,m)
+solve(1)
