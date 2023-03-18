f3c5d395a0ea428a94a943fa628397f07a613501
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-18 17:10:20 +0900

    2667_단지번호붙이기(DFS#2)

--- BAEKJOON/DFS&BFS/2667_단지번호붙이기(DFS).py
+++ BAEKJOON/DFS&BFS/2667_단지번호붙이기(DFS).py
@@ -1,13 +1,15 @@
 
 n = int(input())
 graph = [list(map(int,input())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 cnt = 0
 
 def dfs(x,y):
-    if 0 <= x < n and 0 <= y < n and graph[x][y] == 1:
+    if 0 <= x < n and 0 <= y < n and visited[x][y] == 0 and graph[x][y] == 1:
+        visited[x][y] = 1
         graph[x][y] = 0
         global cnt
         cnt += 1
@@ -22,7 +24,7 @@ def dfs(x,y):
 arr = []
 for i in range(n):
     for j in range(n):
-        if dfs(i,j):
+        if visited[i][j] == 0 and dfs(i,j):
             arr.append(cnt)
         cnt = 0
 
