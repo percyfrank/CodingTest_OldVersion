eb83282c2142b8b5fe9af835e9d6296901265650
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-18 15:18:37 +0900

    11403_경로 찾기(#2)

--- BAEKJOON/DFS&BFS/11403_경로 찾기.py
+++ BAEKJOON/DFS&BFS/11403_경로 찾기.py
@@ -4,31 +4,22 @@ n = int(input())
 graph = []
 for _ in range(n):
     graph.append(list(map(int,input().split())))
-
-a = [[] for _ in range(n+1)]
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            a[i+1].append(j+1)
+result = [[0] * n for _ in range(n)]
 
 def bfs(node):
     q = deque()
     q.append(node)
     while q:
-        node = q.popleft()
-        for next in a[node]:
-            if visited[next] == False:
-                visited[next] = True
-                q.append(next)
+        next = q.popleft()
+        for i in range(n):
+            if not visited[i] and graph[next][i] == 1:
+                visited[i] = True
+                q.append(i)
+                result[node][i] = 1
 
-result = [[0] * n for _ in range(n)]
-
-for i in range(1,n+1):
-    visited = [False] * (n+1)
+for i in range(n):
+    visited = [False] * n
     bfs(i)
-    for j in range(1,n+1):
-        if visited[j] == True:
-            result[i-1][j-1] = 1
 
 for i in result:
     print(*i)
