089f39370c1518df74388480e454e709720c0930
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-23 21:21:09 +0900

    15664_N과 M(10)

---
+++ BAEKJOON/백트래킹/15664_N과 M(10).py
@@ -0,0 +1,21 @@
+
+def solve(start):
+    if len(answer) == m:
+        print(*answer)
+        return
+    tmp = 0
+    for i in range(start,len(arr)):
+        if not visited[i] and tmp != arr[i]:
+            visited[i] = True
+            answer.append(arr[i])
+            tmp = arr[i]
+            solve(i+1)
+            visited[i] = False
+            answer.pop()
+
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+visited = [False] * n
+answer = []
+solve(0)
