17301518f90dbc6592053fd3686cfbdbb51867cf
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-31 11:07:12 +0900

    9095_1, 2, 3 더하기

---
+++ BAEKJOON/DP/9095_1, 2, 3 더하기.py
@@ -0,0 +1,15 @@
+
+t = int(input())
+
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+# 미리 구해두고, 원하는 값을 입력받을 때마다 아래 for문에서 출력하는 것이 나음
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
