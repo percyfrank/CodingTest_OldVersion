61e922c78e74edf683cbf3e8ffd1e8574ce6708a
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 14:24:57 +0900

    6603_로또(복습)

--- BAEKJOON/백트래킹/6603_로또.py
+++ BAEKJOON/백트래킹/6603_로또.py
@@ -1,22 +1,20 @@
 
+
 def solve(start):
     if len(answer) == 6:
         print(*answer)
         return
-    for i in range(start, len(arr)):
-        if not visited[i]:
-            visited[i] = True
-            answer.append(arr[i])
-            solve(i)
-            visited[i] = False
-            answer.pop()
 
-while(True):
+    for i in range(start,len(arr)):
+        answer.append(arr[i])
+        solve(i+1)
+        answer.pop()
+
+while True:
     arr = list(map(int,input().split()))
-    if(arr[0] == 0):
+    if arr[0] == 0:
         break
-    arr = arr[1:len(arr)]
-    visited = [False] * len(arr)
+    arr = arr[1:]
     answer = []
     solve(0)
     print()
