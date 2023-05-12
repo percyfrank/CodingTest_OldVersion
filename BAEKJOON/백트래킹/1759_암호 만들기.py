7f79f84b586e7fb04c692f0bc8e38b17deaf776b
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 14:11:45 +0900

    1759_암호 만들기(복습)

--- BAEKJOON/백트래킹/1759_암호 만들기.py
+++ BAEKJOON/백트래킹/1759_암호 만들기.py
@@ -1,28 +1,27 @@
 
+
 def solve(start):
     if len(answer) == l:
         vo = 0
         co = 0
-        for i in range(l):
-            if answer[i] in answer:
+        for data in answer:
+            if data in vowels:
                 vo += 1
-            else:
-                co += 1
+                continue
+            co += 1
         if vo >= 1 and co >= 2:
             print(''.join(answer))
-            return
+        return
+
     for i in range(start,c):
-        if not visited[i]:
-            visited[i] = True
-            answer.append(arr[i])
-            solve(i)
-            visited[i] = False
-            answer.pop()
+        answer.append(arr[i])
+        solve(i+1)
+        answer.pop()
+
 
 l,c = map(int,input().split())
-vowel = ['a','e','i','o','u']
 arr = list(map(str,input().split()))
 arr.sort()
-visited = [False] * c
+vowels = ['a','e','i','o','u']
 answer = []
 solve(0)
