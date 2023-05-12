bf9549e0dbeca2ba9c7a99df82088dca3ff7d8c7
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 11:56:05 +0900

    1182_부분수열의 합(#2)

--- BAEKJOON/백트래킹/1182_부분수열의 합.py
+++ BAEKJOON/백트래킹/1182_부분수열의 합.py
@@ -1,19 +1,18 @@
 
-
 def solve(start):
     global cnt
-
     if sum(answer) == s and len(answer) > 0:
         cnt += 1
-
-    for num in range(start,n):
-            answer.append(arr[num])
-            solve(num+1)
-            answer.pop()
+    for i in range(start,n):
+        answer.append(arr[i])
+        solve(i+1)
+        answer.pop()
+    return cnt
 
 n,s = map(int,input().split())
 arr = list(map(int,input().split()))
 answer = []
 cnt = 0
-solve(0)
-print(cnt)
+print(solve(0))
+
+
